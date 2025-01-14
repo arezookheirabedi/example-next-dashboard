5a9217f5ac95de12dac713bb5f828984e20f5cd7
“arezookheirabadi” <a_kheirabadi83@yahoo.com>
2025-01-14 17:28:11 +0330

    fix age ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,38 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+'use client';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end   bg-gray-50 p-4 md:h-52">
-         <AcmeLogo /> 
+      <Link
+        className=" flex h-20 items-center justify-center  p-4 md:h-40"
+        href="/dashboard"
+      >
+        <div className="w-32  md:w-40">
+        <Image
+        src="/ZimaLogo-desk.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+            <Image
+        src="/ZimaLogo.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
+      />
+        
+        </div>
+      </Link>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6  bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
+       
           <Link
             href="/dashboard"
             className="flex items-center gap-5 self-start  bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
